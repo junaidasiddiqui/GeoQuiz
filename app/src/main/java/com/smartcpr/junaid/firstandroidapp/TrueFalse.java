55aba1b02901ddaa53ed4bf3678ebab4d3ce0747
junaidasiddiqui <junaidas97@gmail.com>
2017-11-06 16:04:53 -0500

    First App

---
+++ app/src/main/java/com/smartcpr/junaid/firstandroidapp/TrueFalse.java
@@ -0,0 +1,16 @@
+package com.smartcpr.junaid.firstandroidapp;
+
+/**
+ * Created by junaid on 11/6/17.
+ */
+
+public class TrueFalse extends Object {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+}
